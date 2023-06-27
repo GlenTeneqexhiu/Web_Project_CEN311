01efb70bee109f19af36df638e6aaa4d29665f06
irisaNazari <127097325+irisaNazari@users.noreply.github.com>
2023-06-27 17:50:07 +0200

    Add files via upload

---
+++ JS/products.php
@@ -0,0 +1,362 @@
+<?php
+//establish the connection to database, and start the session
+require("includes/common.php");
+?>
+
+<!--Specifies document type is html-->
+<!DOCTYPE html>
+<!--Specifies the language as English-->
+<html lang="en">
+    <head>
+        <!--instructs browser on how to control the page's dimensions and scaling-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--Title of products page-->
+        <title>Products | Bags Store</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <?php
+        include 'includes/header.php';
+        include 'includes/check-if-added.php';
+        ?>
+        <div class="container" id="content">
+            <!-- Jumbotron Header -->
+            <div class="jumbotron home-spacer" id="products-jumbotron">
+                <h1>Welcome to our Bags Store!</h1>
+                <p>We have the best designer bags for you, with a great price. No need to hunt around, we have all in one place.</p>
+
+            </div>
+            <hr>
+
+            <div class="row text-center" id="bags">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/bag1.jpg" alt="">
+                        <div class="caption">
+                            <h3>Prada Bag </h3>
+                            <p>Price: $600 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(1)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=1" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag2.jpg" alt="">
+                        <div class="caption">
+                            <h3>Prada Bag </h3>
+                            <p>Price: $800 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(2)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=2" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag3.jpg" alt="">
+                        <div class="caption">
+                            <h3>Miu Miu Bag </h3>
+                            <p>Price: $200 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(3)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=3" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag4.jpg" alt="">
+                        <div class="caption">
+                            <h3>Balenciaga Bag </h3>
+                            <p>Price: $1000 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(4)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=4" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row text-center" id="bags">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag5.jpg" alt="">
+                        <div class="caption">
+                            <h3>Miu Miu Bag </h3>
+                            <p>Price: $400 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(5)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=5" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag6.jpg" alt="">
+                        <div class="caption">
+                            <h3>Balenciaga Bag </h3>
+                            <p>Price: $750 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(6)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=6" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag7.jpg" alt="">
+                        <div class="caption">
+                            <h3>Prada Bag </h3>
+                            <p>Price: $600 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(7)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=7" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag8.jpg" alt="">
+                        <div class="caption">
+                            <h3>Prada Bag </h3>
+                            <p>Price: $1020 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(8)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=8" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row text-center" id="bags">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag9.jpg" alt="">
+                        <div class="caption">
+                            <h3>Diesel Bag </h3>
+                            <p>Price: $500 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(9)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=9" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag10.jpg" alt="">
+                        <div class="caption">
+                            <h3>D&G Bag </h3>
+                            <p>Price: $1500 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(10)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=10" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag11.jpg" alt="">
+                        <div class="caption">
+                            <h3>Gucci Bag </h3>
+                            <p>Price: $1500 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(11)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=11" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                    <img src="img/bag12.jpg" alt="">
+                        <div class="caption">
+                            <h3>Valentino Bag </h3>
+                            <p>Price: $450 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(12)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=12" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <hr>
+        </div>
+
+        <?php include("includes/footer.php"); ?>
+    </body>
+
+</html>
