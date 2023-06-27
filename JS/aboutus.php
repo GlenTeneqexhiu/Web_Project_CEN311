c8b05dc4110ebb555062d31cf3c08d2a97c5c1b5
Kevin Hyska <127748153+kevinhyska@users.noreply.github.com>
2023-06-27 17:39:40 +0200

    Add files via upload

---
+++ JS/aboutus.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <style type="text/css">
+            h5{
+                color: blue;
+            }
+            .p1{
+                text-align: justify;
+            }
+        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About us | Bags Store</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class="container" id="content">
+    <img src="img/our_team.jpg" style="float: left;">
+    <div class="row">
+        <div class="col-lg-4" align="justify">
+            <h3><mark>WHO WE ARE</mark></h3><br>
+            <p id="p1">Welcome to Bags Store, your ultimate destination for luxury designer bags. At Bags Store, we are passionate about curating an exquisite collection of designer bags that exude style, elegance, and sophistication. With our discerning eye for fashion and commitment to quality, we strive to offer our customers a unique and unforgettable shopping experience.<br><br><h4><mark>Vision</mark></h4>At Bags Store, our vision is to become the go-to destination for fashion-forward individuals seeking premium designer bags. We aim to be recognized as a trusted source for authentic and exclusive designer brands, known for our exceptional customer service and dedication to delivering the finest products.<br><br><h4><mark>Mission</mark></h4>Our mission is to provide our customers with an unparalleled selection of designer bags that showcase the latest trends and timeless classics. We are committed to offering a curated range of brands that represent the epitome of craftsmanship, innovation, and luxury. Our mission is to ensure that every customer finds their perfect bag, a piece that reflects their individual style and enhances their confidence.
+            </p>
+        </div>
+        <div class="col-lg-4" align="justify">
+            <h3><mark>BUILDING THE FUTURE</mark></h3><br>
+            <p id="p1">We strive to have a positive impact on customers, employees, small businesses, the economy, and communities.<br><br> We are smart, passionate builders with different backgrounds and goals, who share a common desire to always be learning and inventing on behalf of our customers.<br><br>We follow the following intensive growth strategies to match our competitors:<br>
+                <ul>
+                    <li>Market development:<br>Our Focus is on entry and growth in new markets.</li><br>
+                    <li>Market Penetration:<br>We aim to generate more revenue from markets where the company currently operates.</li><br>
+                    <li>Product Development:<br>Our goal is to develop and offer new products to gain higher revenues. </li><br>
+                    <li>Diversification:<br>Our objective is to achieve growth based on new business.</li><br> 
+                </ul>
+            </p>
+        </div>
+    </div>
+  </div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
